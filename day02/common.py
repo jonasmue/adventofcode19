0285dc337cc4eeb297201fbf05a36482b8d86da8
Jonas Müller <jonas.mueller@compitencies.com>
2019-12-19 21:18:12 -0800

    Sanitize input

--- day02/common.py
+++ day02/common.py
@@ -18,10 +18,10 @@ def operate(l, start_index, f):
 
 
 def run_program(l, start_index=0):
-    if l[start_index] == 99:
-        return l[0]
-    elif len(l) < start_index + 4:
+    if len(l) <= start_index or len(l) < start_index + 4:
         return -1
+    elif l[start_index] == 99:
+        return l[0]
     elif l[start_index] == 1:
         operate(l, start_index, lambda a, b: a + b)
     elif l[start_index] == 2:
