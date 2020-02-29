caee1b8c79f66836b2fbafd3c13dce1dc2e9e9e8
Jonas Mueller <jonas.jm.mueller@bmwna.com>
2020-02-28 16:09:09 -0800

    Start Day 12 part 2

---
+++ day12/common.py
@@ -0,0 +1,94 @@
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def add(self, other):
+        self.x += other.x
+        self.y += other.y
+        self.z += other.z
+
+    def absolute_sum(self):
+        return abs(self.x) + abs(self.y) + abs(self.z)
+
+    def as_list(self):
+        return [self.x, self.y, self.z]
+
+
+class Moon:
+    def __init__(self, id, position):
+        self.id = id
+        self.position = position
+        self.velocity = Vector(0, 0, 0)
+
+    @staticmethod
+    def get_velocity_summand(u1, u2):
+        return max(min(u2 - u1, 1), -1)
+
+    def update_position(self):
+        self.position.add(self.velocity)
+
+    def update_velocity(self, other):
+        x_increment = Moon.get_velocity_summand(self.position.x, other.position.x)
+        y_increment = Moon.get_velocity_summand(self.position.y, other.position.y)
+        z_increment = Moon.get_velocity_summand(self.position.z, other.position.z)
+        self.velocity.add(Vector(x_increment, y_increment, z_increment))
+
+    def potential_energy(self):
+        return self.position.absolute_sum()
+
+    def kinetic_energy(self):
+        return self.velocity.absolute_sum()
+
+    def total_energy(self):
+        return self.potential_energy() * self.kinetic_energy()
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
+
+
+def get_input(file_name):
+    with open(file_name, 'r') as f:
+        moons = []
+        for i, line in enumerate(f.readlines()):
+            split = line.strip().split(',')
+            x = int(split[0].split('=')[1])
+            y = int(split[1].split('=')[1])
+            z = int(split[2].split('=')[1][:-1])
+            m = Moon(i, Vector(x, y, z))
+            moons.append(m)
+        return moons
+
+
+def update_velocity(moons):
+    for m1 in moons:
+        for m2 in moons:
+            if m1 == m2:
+                continue
+            m1.update_velocity(m2)
+
+
+def update_position(moons):
+    for m in moons:
+        m.update_position()
+
+
+def time_step(moons):
+    update_velocity(moons)
+    update_position(moons)
+
+
+def simulate_motion(moons, n_steps=1000):
+    for i in range(n_steps):
+        time_step(moons)
+
+
+def calculate_system_energy(moons):
+    energy = 0
+    for m in moons:
+        energy += m.total_energy()
+    return energy
---
+++ day12/part2.py
@@ -0,0 +1,38 @@
+from day12.common import *
+import numpy as np
+
+
+def build_position_matrix(moons):
+    matrix = np.zeros([len(moons), 3])
+    for i, m in enumerate(moons):
+        matrix[i] = np.array(m.position.as_list())
+    return matrix
+
+
+def efficient_update_velocity(position_matrix, velocity_matrix, gravity_matrix):
+    sorted_indices = position_matrix.argsort(axis=0)
+    # TODO Sort gravities by indices
+    print("Hej!")
+
+
+def efficient_update_position(position_matrix, velocity_matrix):
+    pass
+
+
+def efficient_time_step(position_matrix, velocity_matrix, gravity_matrix):
+    efficient_update_velocity(position_matrix, velocity_matrix, gravity_matrix)
+    efficient_update_position(moons)
+
+
+if __name__ == '__main__':
+    moons = get_input('input.txt')
+    n_moons = len(moons)
+    velocity_matrix = np.zeros([n_moons, 3])
+    position_matrix = build_position_matrix(moons)
+    gravity_matrix = np.array(
+        [np.array([(n_moons - (i + 1) - ((i + 1) - n_moons)) - (n_moons - 1) for i in range(n_moons)]) for _ in
+         range(3)]).T
+    initial_positions = position_matrix.copy()
+    while True:
+        efficient_time_step(position_matrix, velocity_matrix, gravity_matrix)
+    print("Done")
